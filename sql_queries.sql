53538d823fef5b5be6b761580f3ed7052e8ee9d3
perlajavadevops <72053791+perlajavadevops@users.noreply.github.com>
2023-10-02 09:02:08 +0530

    Added sql quries and JDBC project

--- sql_queries.sql
+++ sql_queries.sql
@@ -23,9 +23,31 @@ insert into emp_details_1 (id, first_name, last_name, salary) values (106, 'Siva
 --MySQL
 create table emp_details_1(id int, first_name varchar(100), last_name varchar(100), salary float, CONSTRAINT id_pk PRIMARY KEY (id));
 insert into emp_details_1 (id, first_name, last_name, salary, email) values (104, 'Siri', 'Peram', 2000, 'abc@gmail.com');
-
+create database batch04;
 commit;
+--MySQL
 
 select * from emp_details_1;
 alter table emp_details_1 add email varchar(30);
 alter table emp_details_1 ADD CONSTRAINT email_unique UNIQUE (email);
+
+insert into emp_details_1 (id, first_name, last_name, salary, email) values (101, 'Mahesh', 'Padmam', 3000, 'mahesh@gmail.com');
+insert into emp_details_1 (id, first_name, last_name, salary, email) values (102, 'Ashok', 'Te', 2000, 'ashok@gmail.com');
+insert into emp_details_1 (id, first_name, last_name, salary, email) values (103, 'Gangadhar', 'Perla', 4000,'gangadhar@gmail.com');
+insert into emp_details_1 (id, first_name, last_name, salary, email) values (104, 'Dhatrika', 'Sai', 5000, 'dhatrika@gmail.com');
+insert into emp_details_1 (id, first_name, last_name, salary, email) values (105, 'Dhruvika', 'Shree', 1000, 'dhruvika@gmail.com');
+insert into emp_details_1 (id, first_name, last_name, salary, email) values (106, 'Siva', 'Reddy', 6000, 'siva@gmail.com');
+
+
+select * from emp_details_1 WHERE FIRST_NAME LIKE '%D%' ORDER BY SALARY ASC;
+
+SELECT Max(SALARY)  FROM emp_details_1  
+SELECT min(SALARY)  FROM emp_details_1  
+SELECT avg(SALARY)  FROM emp_details_1  
+SELECT sum(SALARY)  FROM emp_details_1  
+
+
+select firs from emp_details_1 WHERE id in(101, 102);
+
+select * from emp_details_1 WHERE FIRST_NAME LIKE '%D%';
+
